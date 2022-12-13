bad6f2a667e08342851db2c62ac5b09c0b154834
Vladyslav Tyshchenko <vvant97@gmail.com>
2022-12-13 22:03:07 +0200

    init: initialize project, adjust configuration files

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+
+module.exports = {
+    mode,
+    devtool: devMode ? 'inline-source-map' : undefined,
+    devServer: {
+        static: './dist',
+        port: 8000,
+        watchFiles: ['./src/*'],
+        open: true,
+        hot: true,
+    },
+    entry: './src/app.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[contenthash].bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/[contenthash][ext]',
+                },
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/svg/[contenthash][ext]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[contenthash][ext]',
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    optimization: {
+        minimizer: [new CssMinimizerPlugin()],
+    },
+};
