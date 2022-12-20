9f0dcff6974ee2e5512880ef9de9bae12a4d3bf3
Vladyslav Tyshchenko <vvant97@gmail.com>
2022-12-20 13:40:43 +0200

    feat: add image slider controls

--- src/ts/components/product-card/image-slider.ts
+++ src/ts/components/product-card/image-slider.ts
@@ -10,15 +10,19 @@ const getImageItems = (id: number, type: Image) => {
     const item = document.createElement('li');
     const img = document.createElement('img');
 
-    if (type === 'gallery') {
-      item.dataset.itemId = index.toString();
-    }
-
     item.className = `product-slider__${type}-item`;
     img.className = `product-slider__${type}-image`;
     img.src = source;
     img.alt = alt;
     item.append(img);
+
+    if (type === 'gallery') {
+      item.dataset.itemId = index.toString();
+
+      if (index === 0) {
+        item.classList.add('active');
+      }
+    }
 
     return item;
   });
@@ -33,18 +37,56 @@ const handleSliderMovement = () => {
 
   gallery.addEventListener('click', (event: Event) => {
     const target = (<HTMLElement>event.target).closest('.product-slider__gallery-item') as HTMLElement;
-    const galleryItems = Array.from(gallery.children) as HTMLLIElement[];
-    
+    const galleryItems = Array.from(
+      (<HTMLUListElement>event.currentTarget).querySelectorAll('.product-slider__gallery-item'),
+    );
+
     if (target) {
       const step = item.clientWidth;
-      const index = target.dataset.itemId;
-      
-      if (index) {
-        slider.style.transform = `translateX(-${step * +index}px)`;
-      }
+      const index = <string>target.dataset.itemId;
 
+      slider.style.transform = `translateX(-${step * +index}px)`;
       galleryItems.forEach((item) => item.classList.remove('active'));
       target.classList.add('active');
+    }
+  });
+};
+
+const handleSliderControlsEvents = () => {
+  const gallery = document.querySelector('.product-slider__gallery') as HTMLUListElement;
+
+  gallery.addEventListener('click', (event: Event) => {
+    const target =
+      (<HTMLElement>event.target).matches('.product-slider__gallery-icon-next') ||
+      (<HTMLElement>event.target).matches('.product-slider__gallery-icon-prev');
+    const galleryItems = Array.from(
+      (<HTMLUListElement>event.currentTarget).querySelectorAll('.product-slider__gallery-item'),
+    );
+    const activeSlide = galleryItems.find((slide) => slide.classList.contains('active')) as HTMLLIElement;
+
+    let nextSlide = activeSlide.nextElementSibling as HTMLLIElement;
+
+    if ((<HTMLElement>event.target).matches('.product-slider__gallery-icon-next')) {
+      if (galleryItems.indexOf(activeSlide) === galleryItems.length - 1) {
+        nextSlide = galleryItems[0] as HTMLLIElement;
+      }
+    } else if ((<HTMLElement>event.target).matches('.product-slider__gallery-icon-prev')) {
+      if (galleryItems.indexOf(activeSlide) === 0) {
+        nextSlide = galleryItems.at(-1) as HTMLLIElement;
+      } else {
+        nextSlide = activeSlide.previousElementSibling as HTMLLIElement;
+      }
+    }
+
+    if (target) {
+      const index = <string>nextSlide.dataset.itemId;
+      const slider = document.querySelector('.product-slider__list') as HTMLUListElement;
+      const item = slider.firstElementChild as HTMLLIElement;
+      const step = item.clientWidth;
+
+      activeSlide.classList.remove('active');
+      nextSlide.classList.add('active');
+      slider.style.transform = `translateX(-${step * +index}px)`;
     }
   });
 };
@@ -61,4 +103,5 @@ export const createImageSlider = (id: number) => {
   gallery.append(...getImageItems(id, 'gallery'));
 
   handleSliderMovement();
+  handleSliderControlsEvents();
 };
